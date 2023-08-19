c931b2f05a1d40ed3d0e9fb9f1abf2bd9c04f610
somaiah <somiayaseen59@gmail.com>
2023-08-19 23:55:57 +0300

    v

---
+++ footer.php
@@ -0,0 +1,74 @@
+      <!-- footer section -->
+      <footer class="footer_section">
+         <div class="container">
+            <div class="row">
+             
+            <div class="col-md-3 footer-col">
+                  <!-- <img  src="./images/logo-removebg-preview.png" alt=""> -->
+                  </div>
+
+               <div class="col-md-4 footer-col">
+                  <div class="footer_detail">
+                     <a href="index.html" class="footer-logo">
+                     My light
+                     </a>
+                     <p>
+                     Our diverse lighting range includes ceiling and floor lamps, chandeliers, crystal and pendant lights, wall fixtures, table lamps, downlights, and accessories. Discover our exclusive modern designs
+                     </p>
+                     <div class="footer_social">
+                        <a target="_blank" href="https://web.facebook.com/lightatelier.com.sg?_rdc=1&_rdr">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                     
+                        <a target="_blank" href="https://www.instagram.com/lightatelier.sg/">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                        
+                     </div>
+                  </div>
+               </div>
+               
+               <div class="col-md-5 footer-col">
+                  <div class="footer_contact">
+                     <h4>
+                        Reach at..
+                     </h4>
+                     <div class="contact_link_box">
+                        <a >
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                        <span>
+                        Location : 34 Boon Leat Terrace Singapore 119866
+                        </span>
+                        </a>
+                       
+                        <a >
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                        <span>
+                        hello@lightatelier.com.sg
+                        </span>
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="footer-info">
+               <div class="col-lg-7 mx-auto px-0">
+                  <p>
+                       copyright &copy; 2023 My-light
+   
+                  </p>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- footer section -->
+      <!-- jQery -->
+      <script src="js/jquery-3.4.1.min.js"></script>
+      <!-- popper js -->
+      <script src="js/popper.min.js"></script>
+      <!-- bootstrap js -->
+      <script src="js/bootstrap.js"></script>
+      <!-- custom js -->
+      <script src="js/custom.js"></script>
+   </body>
+</html>
---
+++ index.php
@@ -0,0 +1,458 @@
+
+
+<?php
+
+include("nav.php") ;
+
+?>
+
+
+
+
+
+
+      <div class="hero_area">
+        
+         <!-- slider section -->
+         <section class="slider_section ">
+            <div class="slider_bg_box">
+               <img src="images/slider-bbbg7.jpg" alt="">
+            </div>
+            <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="container ">
+                        <div class="row">
+                           <div class="col-md-7 col-lg-6 ">
+                              <div class="detail-box">
+                                 <h1>
+                                    <span>
+                                    Lighting:                                     
+                                   </span>
+                                    <br>
+                                    More Than Decor!
+                                 </h1>
+                              
+                                 <div class="btn-box">
+                                    <a href="" class="btn1">
+                                    Shop Now
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item ">
+                     <div class="container ">
+                        <div class="row">
+                           <div class="col-md-7 col-lg-6 ">
+                              <div class="detail-box">
+                                 <h1>
+                                    <span>
+                                 
+                                    </span>
+                                    <br>
+                                    a Source of Tranquility 
+                                 </h1>
+                           
+                                 <div class="btn-box">
+                                    <a href="" class="btn1">
+                                    Shop Now
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="container ">
+                        <div class="row">
+                           <div class="col-md-7 col-lg-6 ">
+                              <div class="detail-box">
+                                 <h1>
+                                    <span>
+                                    Style Your Home
+                                    </span>
+                                    <br>
+                                    Soothe Your Soul
+                                 </h1>
+                                 <div class="btn-box">
+                                    <a href="" class="btn1">
+                                    Shop Now
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="container">
+                  <ol class="carousel-indicators">
+                     <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+                     <li data-target="#customCarousel1" data-slide-to="1"></li>
+                     <li data-target="#customCarousel1" data-slide-to="2"></li>
+                  </ol>
+               </div>
+            </div>
+         </section>
+         <!-- end slider section -->
+      </div>
+      <!-- why section -->
+      <section class="why_section layout_padding">
+         <div class="container">
+            <div class="heading_container heading_center">
+               <h2>
+                  Why Shop With Us
+               </h2>
+            </div>
+
+            <div class="row">
+
+            <div class="col-md-3">
+                  <div class="box ">
+                     <div class="img-box">
+                     <i class="fa-solid fa-screwdriver-wrench fa-2xl" style="color: #ffffff;"></i>
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                        Quick Installation
+                        </h5>
+                       
+                     </div>
+                  </div>
+               </div>
+
+               <div class="col-md-3">
+                  <div class="box ">
+                     <div class="img-box">
+                        <i class="fa-solid fa-truck-fast fa-2xl" style="color: #fafcff;"></i>
+                     </div>
+
+                     <div class="detail-box">
+                        <h5>
+                        Free <br> Delivery 
+
+                        </h5>
+                        
+                     </div>
+                  </div>
+               </div>
+
+               <div class="col-md-3">
+                  <div class="box ">
+                     <div class="img-box">
+                     <i class="fa-solid fa-medal fa-2xl" style="color: #fcfcfd;"></i>
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                        Best <br> Quality 
+
+                        </h5>
+
+                     </div>
+                  </div>
+               </div>
+
+               
+
+               <div class="col-md-3">
+                  <div class="box ">
+                     <div class="img-box">
+                     <i class="fa-solid fa-boxes-packing fa-2xl" style="color: #ffffff;"></i>
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                        Echo-Friendly Packing
+                        </h5>
+                       
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end why section -->
+      
+
+     <!-- Categories section -->
+     <section class="product_section layout_padding">
+         <div class="container">
+            <div class="heading_container heading_center">
+               <h2>
+                   Categories
+               </h2>
+            </div>
+            <div class="row">
+
+               <div class="col-sm-6 col-md-6 col-lg-6">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           pendant lamp
+                           </a>
+         
+                        </div>
+                     </div>
+                     <div class="iamg-box">
+                        <img src="./images/cat1.jpg" alt="">
+                     </div>
+                     
+                  </div>
+               </div>
+
+
+               <div class="col-sm-6 col-md-6 col-lg-6">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Wall and Mirror Lamps
+                           </a>
+                          
+                        </div>
+                     </div>
+                     <div class="iamg-box">
+                        <img src="./images/cat5.png" alt="">
+                     </div>
+                     
+                  </div>
+               </div>
+
+
+               <div class="col-sm-6 col-md-6 col-lg-6">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           LED Lighting Strips
+                           </a>
+                           
+                        </div>
+                     </div>
+                     <div class="iamg-box">
+                        <img src="./images/cat6.png" alt="">
+                     </div>
+                     
+                  </div>
+               </div>
+
+
+               <div class="col-sm-6 col-md-6 col-lg-6">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Floor and Task Lamps
+                           </a>
+                           
+                        </div>
+                     </div>
+                     <div class="iamg-box">
+                        <img src="./images/cat3.png" alt="">
+                     </div>
+                     
+                  </div>
+               </div>
+
+             </div>
+            </div>
+         </div>
+      </section>
+      <!-- end Categories section -->
+
+
+
+      
+      <!-- product section -->
+      <section class="product_section layout_padding">
+         <div class="container">
+            <div class="heading_container heading_center">
+               <h2>
+               Discount <span> Section</span>
+               </h2>
+            </div>
+
+            <div class="row">
+
+
+               <div class="col-sm-6 col-md-3 col-lg-3">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           View Product
+                           </a>
+
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p1.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Men's Shirt
+                        </h5>
+                        <h6>
+                           $75
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+
+
+               <div class="col-sm-6 col-md-3  col-lg-3">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           View Product
+                           </a>
+                          
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p2.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Men's Shirt
+                        </h5>
+                        <h6>
+                           $80
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+
+
+               <div class="col-sm-6 col-md-3 col-lg-3">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           View Product
+                           </a>
+                           
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Women's Dress
+                        </h5>
+                        <h6>
+                           $68
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+
+
+               <div class="col-sm-6 col-md-3 col-lg-3">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           View Product
+                           </a>
+                           
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p4.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Women's Dress
+                        </h5>
+                        <h6>
+                           $70
+                        </h6>
+                        <span>
+                           $70
+                        </span>
+                     </div>
+                  </div>
+               </div>
+
+
+            </div>
+            <div class="btn-box">
+               <a href="">
+               View All products
+               </a>
+            </div>
+         </div>
+      </section>
+      <!-- end product section -->
+
+   
+
+
+      <!--start  slider 2 -->
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+           <ol class="carousel-indicators">
+             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+           </ol>
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+            <div id = "img1" style="height:650px;">
+            
+                </div>
+               <div class="carousel-caption d-none d-md-block">
+              <h3 style="color: white;">Brighten Your Life, One Light at a Time: Discover the Comfort in Our Lighting Selection.</h3>
+              <p></p>
+              </div>
+                </div>
+
+          <div class="carousel-item">
+          <div id = "img2" style="height:650px;">
+           
+            </div>
+            <div class="carousel-caption d-none d-md-block">
+            <h3 style="color: black;">Illuminate Your Home with Comfort: Choose our Lighting Solutions for a Happier Space</h3>
+            
+              <p></p>
+            </div>
+            </div>
+
+            <div class="carousel-item">
+          <div id = "img3" style="height:650px;">
+
+            <
+                </div>
+            <div class="carousel-caption d-none d-md-block">
+            <h3 style="color:black ; font-weight: bold;">Transform Your Living Space into a Cozy Haven with Our Relaxing Lighting Options</h3>
+              <p></p>
+            </div>
+          </div>
+
+           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+         </div>
+        </div>
+       
+    <br>
+    <br>
+    <br>
+    <br>
+ 
+    <!-- end slider 2 -->
+
+   
+
