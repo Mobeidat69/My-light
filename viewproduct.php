36d76f2922efa60ebdad51009c40c89a9e1cdc2c
somaiah <somiayaseen59@gmail.com>
2023-08-20 14:56:14 +0300

    f

--- viewproduct.php
+++ viewproduct.php
@@ -45,19 +45,37 @@ include("nav.php") ;
 
 
 
-<div class="container-view">
-        <div class="content-view">
+<div class="container-view" style="background-color: white;
+                                       width: 65rem;
+                                       margin: 30px auto;
+                                       padding: 30px 60px;">
+
+        <div class="content-view" style="background: #dae0e065;
+                                         display: grid;
+                                         ">
         <!-- <h1> VIEW  PRODUCT </h1>  -->
          <br> <br> 
 
-        <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/af09f93881f01035b942f98a24c93478851b3b07/Bella-LED-Brushed-Iron-and-Rose-Gold-Wall-Lamp.jpg" class="" alt="pendant lamp">
+        <img src="https://ak1.ostkcdn.com/images/products/is/images/direct/af09f93881f01035b942f98a24c93478851b3b07/Bella-LED-Brushed-Iron-and-Rose-Gold-Wall-Lamp.jpg" class="" alt="pendant lamp" style=" width: 25rem;
+                                                                                                                                                                                                            height: 35rem;
+                                                                                                                                                                                                            margin-left: 3rem;">
 
-        <div class="text">
+        <div class="text" style="margin-left: 35rem;
+                                 margin-top: -35rem;
+                                 font-size: xx-large;">
         <h2 style="color: #f7444e;"> Wall Lamp  </h2>
         <p style="text-decoration: line-through;"> $135 </p>
         <h3 style="color: #f7444e;"> $100 </h3>
         <p>Bella LED Brushed Iron and Rose Gold Wall Lamp - 8'' x 10''</p>
-        <button><a href="./cart.html"></a> Add to cart </button> 
+        <button style="background-color: #f7444e;
+                       width: 10rem;
+                       height: 4rem;
+                       border-radius: 2rem;
+                       border: none;
+                       font-size: 18px;
+                       color: white;
+                       cursor: pointer;
+                       font-weight: bold;"><a href="./cart.html"></a> Add to cart </button> 
         </div>
 
         <br> <br>
@@ -65,11 +83,25 @@ include("nav.php") ;
         
 
        <h1>Reviews</h1>
-       <div class="Reviews">
+       <div class="Reviews" style="margin-left: 2rem; 
+                                   border: 1px solid black;
+                                   width: 60rem;
+                                   height: 25rem;
+                                   padding-left: 2rem;
+                                   margin-bottom: 3rem;">
         <h2 style="color: #f7444e;">Add your review</h2>
             <form>
-            <textarea name="comment" placeholder="ENTER YOUR COMMENT"> </textarea> <br> <br>
-            <input type="submit"> 
+            <textarea name="comment" placeholder="ENTER YOUR COMMENT" style=" width: 40rem;
+                                                                              height: 10rem;"> </textarea> <br> <br>
+            <input type="submit" style="background-color: #f7444e;
+                                        width: 10rem;
+                                        height: 4rem;
+                                        border: none;
+                                        border-radius: 2rem;
+                                        font-size: 18px;
+                                        color: white;
+                                        cursor: pointer;
+                                        font-weight: bold;"> 
             <form>
     </div>
   
